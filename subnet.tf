e283ed92c72fc34321cec1284711e23aa4291d43
UEmmanuel5 <ugwuemmanuelugo2000@gmail.com>
2025-06-08 04:32:44 +0800

    terraform gitaction commit

---
+++ subnet.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "main_subnet-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.10.128/28"
+  availability_zone = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main_subnet-1"
+  }
+}
+
+
+resource "aws_subnet" "main_subnet-2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.10.144/28"
+  availability_zone = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main_subnet-2"
+  }
+}
