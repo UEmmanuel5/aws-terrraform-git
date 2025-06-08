ada8419a524fe9295eef40fb34cc39876208be76
UEmmanuel5 <ugwuemmanuelugo2000@gmail.com>
2025-06-09 04:45:14 +0800

    added backend.tf and igw.tf, and modified .yaml

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "main_subnet-1" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.main-01.id
   cidr_block = "192.168.10.128/28"
   availability_zone = "ap-southeast-1a"
   map_public_ip_on_launch = true
@@ -11,7 +11,7 @@ resource "aws_subnet" "main_subnet-1" {
 
 
 resource "aws_subnet" "main_subnet-2" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.main-01.id
   cidr_block = "192.168.10.144/28"
   availability_zone = "ap-southeast-1b"
   map_public_ip_on_launch = true
@@ -23,7 +23,7 @@ resource "aws_subnet" "main_subnet-2" {
 
 
 resource "aws_subnet" "main_subnet-3" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.main-01.id
   cidr_block = "192.168.10.160/28"
   availability_zone = "ap-southeast-1c"
   map_public_ip_on_launch = true
