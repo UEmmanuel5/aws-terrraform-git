2413fbce7ed00f740327efd047e33ffe2e1b91c0
UEmmanuel5 <ugwuemmanuelugo2000@gmail.com>
2025-06-09 04:35:58 +0800

    added backend.tf and igw.tf, and modified .yaml

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main-01" {
   cidr_block       = "192.168.10.128/26"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "main-01"
   }
 }
