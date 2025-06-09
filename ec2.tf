ab1eae7725b748f0e44efdeb8dd434e1b06053e2
UEmmanuel5 <ugwuemmanuelugo2000@gmail.com>
2025-06-09 18:57:26 +0800

    website lunch

--- ec2.tf
+++ ec2.tf
@@ -6,13 +6,24 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   key_name                    = "my-key"  # Add this
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt upgrade -y
-              sudo apt install nginx -y
-              sudo systemctl start nginx
-              EOF
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo apt update -y
+#               sudo apt upgrade -y
+#               sudo apt install nginx -y
+#               sudo systemctl start nginx
+#               EOF
+user_data = <<-EOF
+            #!/bin/bash
+            sudo apt update -y
+            sudo apt install nginx unzip wget -y
+            cd /var/www/html
+            sudo rm -rf *
+            wget https://github.com/UEmmanuel5/aws-terrraform-git/archive/refs/heads/main.zip
+            unzip main.zip
+            cp -r aws-terrraform-git-main/website/* /var/www/html/
+            sudo systemctl restart nginx
+            EOF
 
   tags = {
     Name = "web-server"
